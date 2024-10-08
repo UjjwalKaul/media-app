dc5013003c68c22c9905da3ab4711e7806b80e05
Ujjawal Kaul <ujjwal171@outlook.com>
2024-10-08 19:22:39 +0530

    Added nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,15 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Media App</Text>
-      <Link href="/profile">Go to profile</Link>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Media App</Text>
+      <Link href="/profile" className=" text-blue-600">
+        Go to profile
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
