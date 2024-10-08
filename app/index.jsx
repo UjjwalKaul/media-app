720cafff86123f9561d74a62014bc9c84ff91955
Ujjawal Kaul <ujjwal171@outlook.com>
2024-10-08 19:13:45 +0530

    Setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Media App</Text>
+      <Link href="/profile">Go to profile</Link>
       <StatusBar style="auto" />
     </View>
   );
